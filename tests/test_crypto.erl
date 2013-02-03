aa10535060f01b7ea422886244d216cf388c6d10
Gordon <gordon@hypernumbers.com>
2013-02-03 11:37:16 +0000

    updated test fns

--- tests/test_crypto.erl
+++ tests/test_crypto.erl
@@ -8,15 +8,14 @@
 -module(test_crypto).
 
 -export([
-         test/0,
-         test2/0
+         test/0
         ]).
 
 test() ->
     code:add_patha("/home/gordon/hypernumbers/lib/hypernumbers-1.0/ebin"),
     Key  = <<"abcdefghabcdefgh">>,
     IV   = <<"12345678abcdefgh">>,
-    Text = <<"12345678123456781234567812345678">> ,
+    Text = <<"Now is the winter of our discontent made glorious summer by this son of York, ya bas...">> ,
 
     KeySize  = bit_size(Key),
     IVSize   = bit_size(IV),
@@ -26,32 +25,27 @@ test() ->
     io:format("IV   is ~p with size ~p~n", [IV, IVSize]),
     io:format("Text is ~p with size ~p~n", [Text, TextSize]),
 
-    Crypt = crypto:aes_cfb_128_encrypt(Key, IV, Text),
+    PaddedText = extend(Text),
+
+    PaddedSize = bit_size(PaddedText),
+
+    io:format("PaddedText is ~p with size ~p~n", [PaddedText, PaddedSize]),
+
+    Crypt = crypto:aes_cfb_128_encrypt(Key, IV, PaddedText),
     io:format("Crypt is ~p~n", [Crypt]),
     B64 = base64:encode(Crypt),
     io:format("Crypt B64 is ~p~n", [B64]),
 
     Decrypt = crypto:aes_cfb_128_decrypt(Key, IV, Crypt),
     io:format("Decrypt is ~p~n", [Decrypt]),
+    io:format("Decrypted text is ~p~n", [binary_to_list(Decrypt)]),
     ok.
 
-test2() ->
-    code:add_patha("/home/gordon/hypernumbers/lib/hypernumbers-1.0/ebin"),
-    Key = <<97,98,99,100,101,102,103,104,97,98,99,100,101,102,103,104>>,
-    IV   = <<49,50,51,52,53,54,55,56,97,98,99,100,101,102,103,104>>,
-    Text  = <<49,50,51,52,53,54,55,56,49,50,51,52,53,54,55,56>>,
-
-    KeySize  = bit_size(Key),
-    IVSize   = bit_size(IV),
-    TextSize = bit_size(Text),
-
-    io:format("Key  is ~p with size ~p~n", [Key, KeySize]),
-    io:format("IV   is ~p with size ~p~n", [IV, IVSize]),
-    io:format("Text is ~p with size ~p~n", [Text, TextSize]),
-
-    Crypt = crypto:aes_cfb_128_encrypt(Key, IV,Text),
-    io:format("Crypt is ~p~n", [Crypt]),
-
-    Decrypt = crypto:aes_cfb_128_decrypt(Key, IV, Crypt),
-    io:format("Decrypt is ~p~n", [Decrypt]),
-    ok.
+%% Extend binary to a multiple of 128 bits.
+-spec extend(binary()) -> binary().
+extend(Bin) ->
+    Len = size(Bin),
+    io:format("Len is ~p~n", [Len]),
+    Pad = 16 - ((Len + 2) rem 16),
+    io:format("Padding plain text with: ~p~n", [Pad]),
+    <<Len:16, Bin/binary, 0:Pad/unit:8>>.
--- tests/test_crypto.php
+++ tests/test_crypto.php
@@ -1,20 +1,38 @@
 <?php
 
-function dump($String, $Bin) {
-echo $String . " is " . ord($Bin[0]) . " " . ord($Bin[1]) . " " . ord($Bin[2]) . " " . ord($Bin[3]) . " " . ord($Bin[4]) . " " . ord($Bin[5]) . " " . ord($Bin[6]) . " " . ord($Bin[7]) . " " . ord($Bin[8]) . " " . ord($Bin[9]) . " " . ord($Bin[10]) . " " . ord($Bin[11]) . " " . ord($Bin[12]) . " " . ord($Bin[13]) . " " . ord($Bin[14]) . " " . ord($Bin[15]) . " " . ord($Bin[16]) . " " . ord($Bin[17]) . " " . ord($Bin[18]) . " " . ord($Bin[19]) . " " . ord($Bin[20]) . " " . ord($Bin[21]) . " " . ord($Bin[22]) . " " . ord($Bin[23]) . " " . ord($Bin[24]) . " " . ord($Bin[25]) . " " . ord($Bin[26]) . " " . ord($Bin[27]) . " " . ord($Bin[28]) . " " . ord($Bin[29]) . " " . ord($Bin[30]) . " " . ord($Bin[31]) ."\n";
+function dump($Msg, $Binary) {
+		echo $Msg . ":";
+		$Len = strlen($Binary);
+		for ($i = 0; $i < $Len; $i = $i + 1) {
+			echo ord($Binary[$i]) . " ";
+		}
+		echo "\n";
+	}
 
+function extend($Binary) {
+	$Blocksize = 16;
+	$Len = strlen($Binary);
+	echo "Len is " . $Len . "\n";
+	$Pad = $Blocksize - (($Len + 2) % $Blocksize);
+	echo "Padding plain text with:" . $Pad . "\n";
+	$Binary .= str_repeat(chr(0), $Pad);
+	return $Pad . " " . $Binary;
 }
 
 $Key  = "abcdefghabcdefgh";
 $IV   = "12345678abcdefgh";
-$Text = "12345678123456781234567812345678";
+$Text = "Now is the winter of our discontent made glorious summer by this son of York, ya bas...";
 
-$KeySize = strlen($Key) * 8;
-$IVSize = strlen($IV) * 8;
+$KeySize  = strlen($Key) * 8;
+$IVSize   = strlen($IV) * 8;
 $TextSize = strlen($Text) * 8;
 
+$PaddedText = extend($Text);
+
+$PaddedSize = strlen($PaddedText) * 8;
+
 $IVSize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, 'ncfb');
-echo "IVSze is " . $IVSize . " bytes or " . $IVSize * 8 . " bits\n";
+echo "IVSze     is " . $IVSize . " bytes or " . $IVSize * 8 . " bits\n";
 
 $BlockSize = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, 'ncfb');
 echo "BlockSize is " . $BlockSize . " bytes or " . $IVSize * 8 . " bits\n";
@@ -23,10 +41,11 @@ $Crypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $Key, $Text, 'ncfb', $IV);
 
 $UnCrypt = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $Key, $Crypt, 'ncfb', $IV);
 
-echo "Key   is   " . $Key  . " with size " . $KeySize  . "\n";
-echo "IV    is   " . $IV   . " with size " . $IVSize   . "\n";
-echo "Text  is   " . $Text . " with size " . $TextSize . "\n";
-echo "Crypt is " . $Crypt  . "\n";
+echo "Key         is " . $Key        . " with size " . $KeySize  . "\n";
+echo "IV          is " . $IV         . " with size " . $IVSize   . "\n";
+echo "Text        is " . $Text       . " with size " . $TextSize . "\n";
+echo "PaddedText  is " . $PaddedText . " with size " . $PaddedSize . "\n";
+echo "Crypt       is " . $Crypt  . "\n";
 dump("Crypt", $Crypt);
 echo "Decrypt is " . $UnCrypt . "\n";
 dump("Decrypt", $UnCrypt);
